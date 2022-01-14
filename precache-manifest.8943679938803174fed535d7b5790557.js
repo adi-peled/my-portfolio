d9588214f1f25f4f609fe71855a07d7eaf77a7e2
adi <adipeled@gmail.com>
2022-01-14 08:59:19 +0200

    deploy

---
+++ precache-manifest.8943679938803174fed535d7b5790557.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11db0226bf4172e4dd8",
+    "url": "css/app.42af3731.css"
+  },
+  {
+    "revision": "b4e0045784568bc212a3bb6de08891ff",
+    "url": "fonts/Heebo-Medium.b4e00457.ttf"
+  },
+  {
+    "revision": "3f5fef64b9bb3ef6ded20c917f3082f7",
+    "url": "fonts/Heebo-Regular.3f5fef64.ttf"
+  },
+  {
+    "revision": "e30823920fe7f8d820b5a80e54c30c84",
+    "url": "fonts/Heebo-SemiBold.e3082392.ttf"
+  },
+  {
+    "revision": "9ece5b48963bbc96309220952cda38aa",
+    "url": "fonts/Roboto-Bold.9ece5b48.ttf"
+  },
+  {
+    "revision": "6090d256d88dcd7f0244eaa4a3eafbba",
+    "url": "fonts/Roboto-Light.6090d256.ttf"
+  },
+  {
+    "revision": "f36638c2135b71e5a623dca52b611173",
+    "url": "fonts/Roboto-Regular.f36638c2.ttf"
+  },
+  {
+    "revision": "4f0b85f5b601a405bdc7b23aad6d2a47",
+    "url": "fonts/Roboto-Thin.4f0b85f5.ttf"
+  },
+  {
+    "revision": "e72fdf1cca2cebcbe91325bbe9f9e5da",
+    "url": "fonts/RobotoMono-Bold.e72fdf1c.ttf"
+  },
+  {
+    "revision": "9bab8fe7af63fb4a1d536f0690799953",
+    "url": "fonts/RobotoMono-ExtraLight.9bab8fe7.ttf"
+  },
+  {
+    "revision": "fa8ab495d494eccb28f4431f054ddbd4",
+    "url": "fonts/RobotoMono-Light.fa8ab495.ttf"
+  },
+  {
+    "revision": "e5ca8c0ac474df46fe45840707a0c483",
+    "url": "fonts/RobotoMono-Regular.e5ca8c0a.ttf"
+  },
+  {
+    "revision": "a511edd89a551e8f0db621724e1f93bd",
+    "url": "fonts/Rubik-Light.a511edd8.ttf"
+  },
+  {
+    "revision": "6fa3da851e142b514ac7f0e125955e4c",
+    "url": "fonts/Rubik-Medium.6fa3da85.ttf"
+  },
+  {
+    "revision": "4b3f06816033d040ef0ed60865adb2d1",
+    "url": "fonts/Rubik-Regular.4b3f0681.ttf"
+  },
+  {
+    "revision": "a840e539f4f9f5b8ceb038072848ae2f",
+    "url": "fonts/Rubik-SemiBold.a840e539.ttf"
+  },
+  {
+    "revision": "1e92c8e4d37780625b48cb1ca307f2ef",
+    "url": "img/Appsus.1e92c8e4.png"
+  },
+  {
+    "revision": "b763753439da8fbf009c22a55d43b12c",
+    "url": "img/My-Bitcoin-React.b7637534.png"
+  },
+  {
+    "revision": "963f3a33b78dc542659f5225dac258fa",
+    "url": "img/books-shop.963f3a33.png"
+  },
+  {
+    "revision": "b6f306a07643102eaa6daebafa05bfce",
+    "url": "img/chess.b6f306a0.png"
+  },
+  {
+    "revision": "b45e2b1a81fda734b8318a5b35640470",
+    "url": "img/clothing store-1.b45e2b1a.png"
+  },
+  {
+    "revision": "797fe234237b851a1939276452759b0e",
+    "url": "img/clothing store-2.797fe234.png"
+  },
+  {
+    "revision": "8538a4a5b2a7977953a70ce4d4baf058",
+    "url": "img/clothing store-3.8538a4a5.png"
+  },
+  {
+    "revision": "4a7fbb9cd00e5df1637f42b05f53c963",
+    "url": "img/finjan school-1.4a7fbb9c.png"
+  },
+  {
+    "revision": "a4c423b41216df891365ebeef2a9ae97",
+    "url": "img/finjan school-2.a4c423b4.png"
+  },
+  {
+    "revision": "569c936171fc373fe1201276e7b8ca9a",
+    "url": "img/finjan school-3.569c9361.png"
+  },
+  {
+    "revision": "352c2a721b4b720597cfba455e4d3156",
+    "url": "img/finjan school-4.352c2a72.png"
+  },
+  {
+    "revision": "7c859e2800d0e47f0a23802101ec007c",
+    "url": "img/jukebox-1.7c859e28.png"
+  },
+  {
+    "revision": "d2567665269e52a2ce4d3619c0272511",
+    "url": "img/jukebox-2.d2567665.png"
+  },
+  {
+    "revision": "003ca162b457ec1750da1615b90328d1",
+    "url": "img/jukebox-3.003ca162.png"
+  },
+  {
+    "revision": "ebc013c622d3750f4dfe752c81cc992e",
+    "url": "img/jukebox-4.ebc013c6.png"
+  },
+  {
+    "revision": "44c2b1aacbdc1f459915f47ef74a04d6",
+    "url": "img/logo.44c2b1aa.png"
+  },
+  {
+    "revision": "17de6835650653d208310221fc3d0624",
+    "url": "img/meme-generator.17de6835.png"
+  },
+  {
+    "revision": "176c1224bb2f89cb5359103625be45a8",
+    "url": "img/mine-sweeper.176c1224.png"
+  },
+  {
+    "revision": "fc7374ac875b8a9c36c33e184233ab3d",
+    "url": "img/pacman.fc7374ac.png"
+  },
+  {
+    "revision": "d361f5d5eed37798c8114ea4d9703e71",
+    "url": "img/profile.d361f5d5.jpeg"
+  },
+  {
+    "revision": "370f6271e73ded805408b10e5ed11b4a",
+    "url": "img/rcrentals-1.370f6271.png"
+  },
+  {
+    "revision": "8edfb783e26be3dcdb34f6f3a5e43b62",
+    "url": "img/rcrentals-2.8edfb783.png"
+  },
+  {
+    "revision": "7cf213d446125fe92a18651bfc3f8fdb",
+    "url": "img/rcrentals-3.7cf213d4.png"
+  },
+  {
+    "revision": "c0414125643d6b40ed6a2a1f3f3dfa2c",
+    "url": "img/slack-clone.c0414125.png"
+  },
+  {
+    "revision": "502bb858f8c790a2508341b64d3d2001",
+    "url": "index.html"
+  },
+  {
+    "revision": "e11db0226bf4172e4dd8",
+    "url": "js/app.aed13e4f.js"
+  },
+  {
+    "revision": "7fff8b2d21bebcaa37f3",
+    "url": "js/chunk-vendors.ada221c3.js"
+  },
+  {
+    "revision": "393ed8967b2ebc0f947c2ae90e45edb3",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
